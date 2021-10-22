07bfbacd29ba215da06bf23e321cab85f370b375
Cameron <cameronkwalden@gmail.com>
2021-10-21 20:14:31 -0700

    initial commit

---
+++ src/models/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+require('dotenv').config();
+
+//this connects us to the database
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory';
+const { Sequelize, DataTypes } = require('sequelize');
+const bookModel = require('./books.js');
+
+const movieModel = require('./movies.js')
+
+const options = process.env.NODE_ENV === 'production'
+  ? {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
+  }
+  : {};
+
+
+let sequelizeInstance = new Sequelize('sqlite:memory');
+const bookTable = bookModel(sequelizeInstance, DataTypes);
+const movieTable = movieModel(sequelizeInstance, DataTypes);
+
+// const bookCollection = new Collection(bookTable);
+// const movieCollection = new Collection(movieTable)
+
+module.exports = {
+  db: sequelizeInstance,
+  book: bookTable,
+  movie: movieTable,
+}
